054077b59dde683a55b95a521f3c9e8071a26216
Haofphm4426 <haofphm4426@gmail.com>
2023-10-12 16:26:15 +0700

    error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,14 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 
-const createNew = async (req, res) => {
+const createNew = async (req, res, next) => {
   try {
-    console.log(req.body);
     res.status(StatusCodes.OK).json({ message: 'POST from Validation: API create new board' });
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    });
-  }
+
+  } catch (error) { next(error); }
 };
 
 export const boardController = {
--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ import express from 'express';
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb';
 import { env } from './config/environment';
-import { APIs_V1 } from '~/routes/v1'
+import { APIs_V1 } from '~/routes/v1';
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware';
 
 const START_SERVER = () => {
   const app = express();
@@ -17,6 +18,9 @@ const START_SERVER = () => {
   app.use(express.json());
 
   app.use('/v1', APIs_V1);
+
+  //Middleware xử lý lỗi
+  app.use(errorHandlingMiddleware);
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello ${env.AUTHOR}, I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`);
