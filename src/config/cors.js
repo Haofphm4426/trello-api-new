3db75105eb926bae70e577bc69a074519062d4fd
Haofphm4426 <haofphm4426@gmail.com>
2023-11-24 09:17:32 +0700

    aggregate

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,6 +6,7 @@ import ApiError from '~/utils/ApiError';
 // Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
 export const corsOptions = {
   origin: function (origin, callback) {
+    console.log('orgin: ', origin);
     // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
     // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
     if (!origin && env.BUILD_MODE === 'dev') {
